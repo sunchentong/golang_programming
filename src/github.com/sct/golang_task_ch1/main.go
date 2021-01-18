b1df2211473cf137949424a9a04fb9608994c72d
sunchentong <esteemed@126.com>
2021-01-18 23:36:14 +0800

    [description] add sample_dup code
    Signed-off-by: sunchentong <esteemed@126.com>

--- src/github.com/sct/golang_task_ch1/main.go
+++ src/github.com/sct/golang_task_ch1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -37,6 +38,38 @@ func test13(args []string) {
 	fmt.Printf("%.4fs elapsed \n", time.Since(start).Seconds())
 }
 
+func sampleDup() {
+	fmt.Printf("-- Sample Dup \n")
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Printf("open %s failed, err : %v \n", arg, err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("filename : %s \n", os.Args[0])
+			fmt.Printf("line :%s , n : %d \n", line, n)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
+
 func main() {
 	// exercise 1.1
 	test11(os.Args[0])
@@ -46,4 +79,7 @@ func main() {
 
 	// exercise 1.3
 	test13(os.Args[1:])
+
+	// sample dup1
+	sampleDup()
 }
